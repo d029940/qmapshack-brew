e614698776e5140e909766c359d6b99116693b7c
Manfred Kern <manfred.kern@gmail.com>
2023-03-08 21:09:36 +0100

    update to new url

--- qmapshack.rb
+++ qmapshack.rb
@@ -4,12 +4,11 @@ cask "qmapshack" do
   # version "1.16.1"
   # sha256 '967881d88a09964661ea0f181ae29c06b1f771a3047cf9a50683f3d5eecfd15f'
 
-  url "https://github.com/d029940/qmapshack/releases/download/QMapShack-Mac/QMapShack_1.16.1-2-arm-brew.zip"
+  url "https://github.com/d029940/qmapshack-brew/releases/download/QMapShack-Mac/QMapShack_1.16.1-2-arm-brew.zip"
   name "QMapShack"
   desc "GPS planning tool"
-  homepage  "https://github.com/d029940/qmapshack/"
-  # homepage  "https://github.com/Maproom/qmapshack/wiki"
-  # license :gpl
+  # homepage  "https://github.com/d029940/qmapshack-brew/"
+  homepage  "https://github.com/Maproom/qmapshack/wiki"
 
 
   depends_on formula: [ 
